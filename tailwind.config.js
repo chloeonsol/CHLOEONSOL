6166221c12da01b8e51cae1416e337636a0ba3ba
chloeonsol <chloeonsol@gmail.com>
2025-02-18 12:18:34 +0100

    Correcciones

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", 
+  ],
   theme: {
-    extend: {
-      colors: {
-        chloePurple: "#7D3C98",
-        chloeWhite: "#FFFFFF",
-        chloeLightPurple: "#9B59B6",
-        chloeDarkPurple: "#5D2A9F",
-      },
-      fontFamily: {
-        sans: ["'Poppins'", "sans-serif"],
-        display: ["'Bebas Neue'", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
